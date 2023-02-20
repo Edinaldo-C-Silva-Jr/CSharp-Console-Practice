c56d5802dfa600e35ff78c4854a1adbeced9d208
Edinaldo C da Silva Jr <edinaldo.csilvajr@outlook.com>
2023-02-20 16:30:49 -0300

    Changed text descend speed calculation. Added comments for documentation.

--- Random Minor Projects/TextManipulation/DescendingText/Program.cs
+++ Random Minor Projects/TextManipulation/DescendingText/Program.cs
@@ -15,35 +15,36 @@ namespace DescendingText
 			int cursorX = 0, descendValue = 0;
 			
 			Console.WriteLine("Digite um texto qualquer: ");
-			//Input method
+			//Input method (1 a 80)
 			textToDescend = Console.ReadLine();
 			Console.WriteLine("Digite a distância que o texto irá cair: ");
-			//Input method
+			//Input method (1 a 23)
 			descendValue = int.Parse(Console.ReadLine());
 			
-			if (textToDescend == "")
+			if (textToDescend == "") // Easter Egg condition to not allow a blank text to be entered
 			{
-				textToDescend = "Hello World!";
+				textToDescend = "Hello World!"; // Every program needs a Hello World
 			}
 			
 			Console.Clear();
 			
-			cursorX = (Console.BufferWidth - textToDescend.Length) / 2;
-			Console.SetCursorPosition(cursorX, 0);
+			cursorX = (Console.BufferWidth - textToDescend.Length) / 2; // Sets the horizontal position of the cursor based on the text length and buffer width
+			Console.SetCursorPosition(cursorX, 0); // Centralizes the text on the screen no regardless of text or buffer size
 			Console.WriteLine(textToDescend);
 			Thread.Sleep(1000);
 			
-			for(int i = 0; i < textToDescend.Length; i++)
+			for(int i = 0; i < textToDescend.Length; i++) // Each iteration refers to one character of the text
 			{
-				if (textToDescend.Substring(i, 1) != " ")
+				if (textToDescend.Substring(i, 1) != " ") // Skips descending process if the character
 				{
-					for(int j = 0; j < descendValue; j++)
+					for(int j = 0; j < descendValue; j++) // Each iteration refers to one line of text descending
 					{
-						Console.SetCursorPosition(cursorX + i, j);
+						Console.SetCursorPosition(cursorX + i, j); // Makes current position blank
 						Console.Write(" ");
-						Console.SetCursorPosition(cursorX + i, j + 1);
+						Console.SetCursorPosition(cursorX + i, j + 1); // Writes character on the position below
 						Console.WriteLine(textToDescend.Substring(i, 1));
-						Thread.Sleep(500 / (((4 + textToDescend.Length) / 4) + ((2 + descendValue) / 2)));
+						
+						Thread.Sleep(500 / (((4 + textToDescend.Length) / 4) * ((3 + descendValue) / 3))); // Timer to wait between each descend step. Based on both the text lenght and descending distance, essentially increasing the text speed when there are more steps.
 					}
 				}
 			}

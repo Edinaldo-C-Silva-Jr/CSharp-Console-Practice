5ce47f575cdf8eac1db727445c9542d7838c2562
Edinaldo C da Silva Jr <edinaldo.csilvajr@outlook.com>
2023-03-21 18:27:21 -0300

    Made some more fixes to the ValidateInput class library. Made all its methods and attributes be instanced instead of static. Updated the TestValidation project to include the new ValidateChar method. Also made TestValidation use the new validation method names.

--- Test Projects/TestValidation/TestInputValidation/Program.cs
+++ Test Projects/TestValidation/TestInputValidation/Program.cs
@@ -16,85 +16,97 @@ namespace TestInputValidation
 			int integer;
 			double number;
 			string text;
+			char character;
+			
+			ValidateInput validation = new ValidateInput();
 			
 			// Testing the validation of a string value, matching it with an array of expected inputs
-			// The array of expected inputs has to be set (with the SetValidValues method) before the ValidInput method can be used. Otherwise it will work exactly like a Console.ReadLine() and allow any input.
-			// The method SetValieValues is not case sensitive
+			// The array of expected inputs has to be set (with the SetValidStrings method) before the ValidateString method can be used. Otherwise it will work exactly like a Console.ReadLine() and allow any input.
+			// The method SetValidStrings is not case sensitive
 			Console.WriteLine("Digite 'sim' ou 'nao' (pode utilizar apenas as iniciais): ");
-			ValidateInput.SetValidValues(new string[] {"sim", "s", "nao", "não", "n"}); // Sets the ValidValues array to the values considered valid for this test
-			text = ValidateInput.ValidInput(); // Asks for an input and keeps trying until the text entered corresponds to one of the values decided above
+			validation.SetValidStrings(new string[] {"sim", "s", "nao", "não", "n"}); // Sets the ValidStrings array to the values considered valid for this test
+			text = validation.ValidateString(); // Asks for an input and keeps trying until the text entered corresponds to one of the values decided above
 			Console.WriteLine("Valor válido: " + text);
 			Thread.Sleep(1000);
 			
 			// Testing the validation of a specific string value
 			Console.WriteLine("\nDigite a frase 'O rato roeu a roupa do rei de Roma':");
-			ValidateInput.SetValidValues(new string[] {"O rato roeu a roupa do rei de Roma"}); // Sets the ValidValues array with a new (and very specific) value for this test
-			text = ValidateInput.ValidInput();
+			validation.SetValidStrings(new string[] {"O rato roeu a roupa do rei de Roma"}); // Sets the ValidStrings array with a new (and very specific) value for this test
+			text = validation.ValidateString();
 			Console.WriteLine("Valor válido: " + text);
 			Thread.Sleep(1000);
 			
-			// Testing the validation of an integer value
+			// Testing the validation of a single character
+			// The array of expected inputs has to eb set (with the SetValidChars method) before the ValidateChar method can be used. Otherwise it will work exactly like a Console.ReadKey() and allow any input
+			// The method SetValidChars is not case sensitive 
+			Console.WriteLine("\nDigite uma vogal (A, E, I, O ou U):");
+			validation.SetValidChars(new char[] {'a', 'e', 'i', 'o', 'u'}); // Sets the ValidChars array with the values considered valid for this test
+			character = validation.ValidateChar();
+			Console.WriteLine("Valor válido: " + character);
+			Thread.Sleep(1000);
+			
+			// Testing the validation of a 32-bit integer value
 			Console.WriteLine("\nDigite um número inteiro (int 32 bit):");
-			integer = ValidateInput.ValidInt(); // Asks for an input and keeps trying until a 32 bit integer is entered
+			integer = validation.ValidateInt(); // Asks for an input and keeps trying until a 32 bit integer is entered
 			Console.WriteLine("Valor válido: " + integer);
 			Thread.Sleep(1000);
 			
-			// Testing the validation of an integer value that cannot be 0
+			// Testing the validation of a 32-bit integer value that cannot be 0
 			Console.WriteLine("\nDigite um número inteiro (int 32 bit). Deve ser diferente de 0:");
-			integer = ValidateInput.ValidIntNotZero(); // Asks for an input and keeps trying until a 32 bit integer is entered
+			integer = validation.ValidateIntNotZero(); // Asks for an input and keeps trying until a 32 bit integer, that is not 0, is entered
 			Console.WriteLine("Valor válido: " + integer);
 			Thread.Sleep(1000);
 			
-			// Testing the validation of a double value
+			// Testing the validation of a numeric value (double variable)
 			Console.WriteLine("\nDigite um número qualquer: ");
-			number = ValidateInput.ValidDouble(); // Asks for an input and keeps trying until a numeric (double) value is entered
+			number = validation.ValidateDouble(); // Asks for an input and keeps trying until a numeric value is entered
 			Console.WriteLine("Valor válido: " + number);
 			Thread.Sleep(1000);
 			
-			// Testing the validation of a double value that cannot be 0
+			// Testing the validation of a numeric value that cannot be 0
 			Console.WriteLine("\nDigite um número qualquer. Deve ser diferente de 0: ");
-			number = ValidateInput.ValidDoubleNotZero(); // Asks for an input and keeps trying until a numeric (double) value is entered
+			number = validation.ValidateDoubleNotZero(); // Asks for an input and keeps trying until a numeric value, that is not 0, is entered
 			Console.WriteLine("Valor válido: " + number);
 			Thread.Sleep(1000);
 			
 			// Testing the validation of a string value, but this time based on the amount of characters entered. 
 			// The parameters passed to the method indicate the character amount allowed, defining both the minimum and maximum bounds for the amount of characters allowed. Note that, while the first is the minimum and the second is the maximum bound, the method swaps them in case the minimum is the higher bound
-			// Note that the minimum parameter cannot be higher than 200, and the maximum cannot be lower than 1
+			// Note that the minimum parameter cannot be higher than 253, and the maximum cannot be lower than 1
 			Console.WriteLine("\nDigite uma frase entre 20 e 50 caracteres: ");
-			text = ValidateInput.ValidInputSize(20, 50); // Asks for an input and keeps trying until the entered text has a specific length (In this case, between 20 and 50 characters)
+			text = validation.ValidateInputSize(20, 50); // Asks for an input and keeps trying until the entered text has a specific length (In this case, between 20 and 50 characters)
 			Console.WriteLine("Valor válido: " + text);
 			Thread.Sleep(1000);
 			
 			// Testing the way the method handles when the minimum bound is set higher than the maximum
 			Console.WriteLine("\nDigite uma frase entre 10 e 5 caracteres: ");
-			text = ValidateInput.ValidInputSize(10, 5); 
+			text = validation.ValidateInputSize(10, 5); // Despite the minimum bound being higher, the method should still correctly accept an input between 5 and 10 characters
 			Console.WriteLine("Valor válido: " + text);
 			Thread.Sleep(1000);
 			
 			// Testing the validation of a numeric input, based on the value of the number itself
 			// The parameters passed to the method indicate the range of numbers allowed. Note that, while the first is the minimum and the second is the maximum value, the method swaps them in case the minimum is the higher value
 			Console.WriteLine("\nDigite um número entre 3859 e 8465: ");
-			number = ValidateInput.ValidNumericValue(3859, 8465); // Asks for an input and keeps trying until the entered number has a value inside of a specific range (In this case, between 3859 and 8465)
+			number = validation.ValidateNumericValue(3859, 8465); // Asks for an input and keeps trying until the entered number has a value inside of a specific range (In this case, between 3859 and 8465)
 			Console.WriteLine("Valor válido: " + number);
 			Thread.Sleep(1000);
 			
 			// Testing the way the method handles when the minimum value is higher than the maximum
 			Console.WriteLine("\nDigite um número entre 3000 e 2000: ");
-			number = ValidateInput.ValidNumericValue(3000, 2000); 
+			number = validation.ValidateNumericValue(3000, 2000); // Despite the minimum value being higher, the method should still correctly accept a value between 2000 and 3000
 			Console.WriteLine("Valor válido: " + number);
 			Thread.Sleep(1000);
 			
 			// Testing the validation of a numeric input based on its sign
-			// The parameter passed to the method indicate if the valid numbers should be positive or negative. Passing "true" means the method will validate for positive numbers
+			// The parameter passed to the method indicates if the valid numbers should be positive or negative. Passing "true" means the method will validate for positive numbers
 			Console.WriteLine("\nDigite um número positivo: ");
-			number = ValidateInput.ValidNumberSign(true); // Asks for an input and keeps trying until the entered number is positive
+			number = validation.ValidateNumberSign(true); // Asks for an input and keeps trying until the entered number is positive
 			Console.WriteLine("valor válido: " + number);
 			Thread.Sleep(1000);
 			
 			// Testing the validation of a numeric input based on its sign
 			// The parameter passed to the method indicate if the valid numbers should be positive or negative. Passing "false" means the method will validate for negative numbers
 			Console.WriteLine("\nDigite um número negativo: ");
-			number = ValidateInput.ValidNumberSign(false); // Asks for an input and keeps trying until the entered number is negative
+			number = validation.ValidateNumberSign(false); // Asks for an input and keeps trying until the entered number is negative
 			Console.WriteLine("valor válido: " + number);
 			Thread.Sleep(1000);
 			
--- _Class Libraries_/Validation/InputValidation/Program.cs
+++ _Class Libraries_/Validation/InputValidation/Program.cs
@@ -11,18 +11,18 @@ namespace InputValidation
 	// These methods ensure the inputted value will be in the desired format in order to avoid parsing errors or unintended behaviors
 	public class ValidateInput
 	{
-		private static int cursorX = 0, cursorY = 0; // Cursor position values - Used in the method ClearPreviousLine()
+		private int cursorX = 0, cursorY = 0; // Cursor position values - Used in the method ClearPreviousLine()
 		
-		private static string[] validStrings = new string[] {}; // String that contains the values which are considered valid - Used in the method ValidateString()
-		private static char[] validChars = new char[] {}; // Array that contains the char values that are considered valid - Used in the method ValidateChar()
-		private static string inputToTest = "";
-		private static char charToTest = ' ';
-		private static double numberToTest = 0;
-		private static int integerToTest = 0;
+		private string[] validStrings = new string[] {}; // Array that contains the values which are considered valid - Used in the method ValidateString()
+		private char[] validChars = new char[] {}; // Array that contains the char values that are considered valid - Used in the method ValidateChar()
+		private string inputToTest = "";
+		private char charToTest = ' ';
+		private double numberToTest = 0;
+		private int integerToTest = 0;
 		
 		// Method that gets the current cursor position in the console window
 		// Used to know the position the cursor should return to in the method below
-		private static void GetCursorPosition()
+		private void GetCursorPosition()
 		{
 				cursorX = Console.CursorLeft;
 				cursorY = Console.CursorTop;
@@ -30,7 +30,7 @@ namespace InputValidation
 		
 		// Method that clears the previous line of the console
 		// Intended to be used with the validation methods to restore the cursor to its original position in case an input was invalid
-		private static void ClearPreviousLine(int inputLength)
+		private void ClearPreviousLine(int inputLength)
 		{
 			Console.SetCursorPosition(cursorX, cursorY); // Sets the cursor back to the position recorded before the input
  			Console.Write(new string(' ', inputLength)); // Builds a string of blank spaces to erase previous input
@@ -40,7 +40,7 @@ namespace InputValidation
 		
 		// Sets the valid values for the method ValidateString
 		// Converts the received string to lowercase, so the ValidateString method works regardless of case
-		public static void SetValidStrings(string[] valid)
+		public void SetValidStrings(string[] valid)
 		{
 			validStrings = Array.ConvertAll(valid, individualValue => individualValue.ToLower());
 		}
@@ -48,7 +48,7 @@ namespace InputValidation
 		// Method that repeatedly asks for an input until the inputted value is contained in the "validStrings" array. Intended to be used in situations where the program only accepts specific values
 		// Example: The user has to answer a question with only "yes" or "no", thus the "validStrings" array would be set to {"yes", "no"}
 		// Note: If ValidStrings is empty, the method will accept any input (essentially the same as just using a Console.ReadLine())
-		public static string ValidateString()
+		public string ValidateString()
 		{	
 			do
 			{
@@ -73,7 +73,7 @@ namespace InputValidation
 		
 		// Sets the valid values for the method ValidateChar
 		// Converts the received chars to lowercase, so the ValidateChar method works regardless of case
-		public static void SetValidChars(char[] valid)
+		public void SetValidChars(char[] valid)
 		{
 			validChars = Array.ConvertAll(valid, individualValue => Char.ToLower(individualValue));
 		}
@@ -81,7 +81,7 @@ namespace InputValidation
 		// Method that repeatedly asks for a char input until the inputted value is contained in the "validChars" array. Intended to be used in situations where the program should only accept specific values in a single character input (Using a Console.ReadKey(), as opposed to a Console.ReadLine() used in the previous method)
 		// Example: The user has to answer with 'y' or 'n', for "yes" and "no", thus the "validChars" array would be set to {'y', 'n'}
 		// Note: If validChars is empty, the method will accept any input (essentially the same as just using a Console.ReadKey())
-		public static char ValidateChar()
+		public char ValidateChar()
 		{
 			do
 			{
@@ -105,7 +105,7 @@ namespace InputValidation
 		}
 		
 		// Method that repeatedly asks for an input until the inputted value is a 32-bit integer
-		public static int ValidateInt()
+		public int ValidateInt()
 		{
 			bool isValid = false; // Variable that stores whether the input was valid or not (it receives the boolean result from the TryParse method)
 			
@@ -131,7 +131,7 @@ namespace InputValidation
 		}
 		
 		// Method that repeatedly asks for an input until the inputted value is a 32-bit integer and is not 0
-		public static int ValidateIntNotZero()
+		public int ValidateIntNotZero()
 		{
 			do
 			{
@@ -152,7 +152,7 @@ namespace InputValidation
 		}
 		
 		// Method that repeatedly asks for an input until the inputted value is a number (double variable)
-		public static double ValidateDouble()
+		public double ValidateDouble()
 		{
 			bool isValid = false; // Variable that stores whether the input was valid or not (it receives the boolean result of the TryParse method)
 			
@@ -178,7 +178,7 @@ namespace InputValidation
 		}
 		
 		// Method that repeatedly asks for an input until the inputted value is a number (double variable) and is not 0
-		public static double ValidateDoubleNotZero()
+		public double ValidateDoubleNotZero()
 		{
 			do
 			{
@@ -201,7 +201,7 @@ namespace InputValidation
 		// Method that repeatedly asks for input until the inputted value has the desired length, which is defined by the parameters minBound and maxBound
 		// The method is set to swap the bounds in case the minBound is bigger than the maxBound, to prevent getting stuck in an impossible validation
 		// It also has a limit to the bounds, maxBound cannot be lower than 1 and minBound cannot be higher than 253
-		public static string ValidateInputSize (int minBound, int maxBound)
+		public string ValidateInputSize (int minBound, int maxBound)
 		{
 			if (minBound > maxBound) // Checks if the minBound is bigger than the maxBound. If it is, swap them around to prevent the method from breaking (since it's impossible for a number to be within the range if the minimum is higher than the maximum)
 			{
@@ -243,7 +243,7 @@ namespace InputValidation
 		
 		// Method that repeatedly asks for numeric inputs until the inputted number is within a specified range of values, defined by the parameters minValue and maxValue
 		// The method is set to swap the values in case the minValue is bigger than the maxValue, to prevent getting stuck in an impossible validation
-		public static double ValidateNumericValue(double minValue, double maxValue)
+		public double ValidateNumericValue(double minValue, double maxValue)
 		{	
 			if (minValue > maxValue) // Checks if the minValue is actually bigger than the maxValue. If it is, swap them around to prevent the method from breaking (since it's impossible for a number to be within the range if the minimum is bigger than the maximum)
 			{
@@ -272,7 +272,7 @@ namespace InputValidation
 		
 		// Method that repeatedly asks for input until the number entered has the correct sign
 		// The bool parameter controls the sign required. If the parameter is true, only positive numbers are accepted, if it's false, only negative numbers are accepted
-		public static double ValidateNumberSign (bool positiveNumber)
+		public double ValidateNumberSign (bool positiveNumber)
 		{
 			do
 			{

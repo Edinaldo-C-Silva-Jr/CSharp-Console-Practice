d9ab5bd4c251e32ced693e19613a44400f0d2b86
Edinaldo C da Silva Jr <edinaldo.csilvajr@outlook.com>
2023-03-28 22:58:55 -0300

    Created a Tic Tac Toe project. Or rather, i am recreating one i made a few months ago to save into the repository. Not fully functional yet, but there's a foundation for the game playing field and the players taking turns.

---
+++ TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Date: 28/03/2023
+ * Time: 18:57
+ */
+using System;
+
+namespace TicTacToe
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.SetWindowSize(49, 20);
+			Console.SetBufferSize(49, 20);
+			TicTacToe game = new TicTacToe();
+			
+			do
+			{
+				Console.WriteLine("---------- Jogo da Velha ----------");
+				Console.WriteLine("Escolha o número de jogadores (1 ou 2): ");
+				
+				game.SetOpponentMode(); // Will be changed to validation class
+				game.ResetGame();
+				game.DrawPlayField();
+				
+				game.PlayGame();
+				
+				Console.ReadKey();
+			}
+			while (true); // Eventually will be "while input != esc"
+		}
+	}
+}
---
+++ TicTacToe/TicTacToe/TicTacToe.cs
@@ -0,0 +1,95 @@
+﻿/*
+ * Date: 28/03/2023
+ * Time: 22:18
+ */
+using System;
+
+namespace TicTacToe
+{
+	/// <summary>
+	/// Description of TicTacToe.
+	/// </summary>
+	public class TicTacToe
+	{
+		private char[] gameFields = new char[9];
+		private bool againstComputer, isPlayerOneTurn, winnerFound;
+		private int positionPlayed, currentTurn, victoryCombination;
+		
+		public TicTacToe()
+		{
+		}
+		
+		public void ResetGame()
+		{
+			for (int i = 0; i < 9; i++) {
+				this.gameFields[i] = ' ';
+			}
+			
+			this.currentTurn = 0;
+			this.victoryCombination = 0;
+			this.winnerFound = false;
+			this.isPlayerOneTurn = false;
+		}
+		
+		public void DrawPlayField()
+		{
+			Console.Clear();
+			Console.SetCursorPosition(12, 0);
+			Console.Write("----- Jogo da Velha -----");
+			Console.SetCursorPosition(0, 2);
+			Console.WriteLine("     ---------------------------------------");
+			Console.WriteLine("     |[1]    " + this.gameFields[0] + "    |[2]    " + this.gameFields[1] + "    |[3]    " + this.gameFields[2] + "   |");
+			Console.WriteLine("     |[4]    " + this.gameFields[3] + "    |[5]    " + this.gameFields[4] + "    |[6]    " + this.gameFields[5] + "   |");
+			Console.WriteLine("     |[7]    " + this.gameFields[6] + "    |[8]    " + this.gameFields[7] + "    |[9]    " + this.gameFields[8] + "   |");
+			Console.WriteLine("     ---------------------------------------\n");
+		}
+		
+		public void SetOpponentMode()
+		{
+			int players = int.Parse(Console.ReadLine());
+			this.againstComputer = (players == 1);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		public void PlayGame()
+		{
+			while(!(winnerFound))
+			{
+				isPlayerOneTurn = !isPlayerOneTurn;
+				
+				if (isPlayerOneTurn)
+				{
+					Console.WriteLine("Jogador 1, escolha a posição");
+					positionPlayed = int.Parse(Console.ReadLine()); // Will be changed to validation class
+					
+					gameFields[positionPlayed - 1] = 'X';
+				}
+				else
+				{
+					if (againstComputer)
+					{
+						
+					}
+					else
+					{
+						Console.WriteLine("Jogador 2, escolha a posição");
+						positionPlayed = int.Parse(Console.ReadLine()); // Will be changed to validation class
+						
+						gameFields[positionPlayed - 1] = 'O';
+					}
+				}
+				
+				
+				DrawPlayField();
+				currentTurn++;
+			}
+		}
+	}
+}

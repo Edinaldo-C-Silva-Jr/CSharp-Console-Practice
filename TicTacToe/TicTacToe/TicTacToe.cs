60515f6e5484a5985a2ddf2f6c0dbcac654d44a5
Edinaldo C da Silva Jr <edinaldo.csilvajr@outlook.com>
2023-03-30 17:57:17 -0300

    Finished the main features of the Tic Tac Toe project. It's now fully playable, properly shows the winning player and winning line, and has a basic easy computer AI. It also validates the numeric inputs.

--- TicTacToe/TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe/TicTacToe.cs
@@ -3,6 +3,8 @@
  * Time: 22:18
  */
 using System;
+using System.Threading;
+using InputValidation;
 
 namespace TicTacToe
 {
@@ -12,17 +14,20 @@ namespace TicTacToe
 	public class TicTacToe
 	{
 		private char[] gameFields = new char[9];
+		private string[] drawnFields = new string[9];
 		private bool againstComputer, isPlayerOneTurn, winnerFound;
-		private int positionPlayed, currentTurn, victoryCombination;
+		private int positionPlayed, currentTurn;
+		
+		ValidateInput validation = new ValidateInput();
 		
 		private void ResetGame()
 		{
 			for (int i = 0; i < 9; i++) {
 				this.gameFields[i] = ' ';
+				this.drawnFields[i] = "         ";
 			}
 			
 			this.currentTurn = 0;
-			this.victoryCombination = 0;
 			this.winnerFound = false;
 			this.isPlayerOneTurn = false;
 		}
@@ -32,37 +37,61 @@ namespace TicTacToe
 			Console.Clear();
 			Console.SetCursorPosition(12, 0);
 			Console.Write("----- Jogo da Velha -----");
+			
+			RedrawPlayField();
+		}
+		
+		private void RedrawPlayField()
+		{
 			Console.SetCursorPosition(0, 2);
-			Console.WriteLine("     ---------------------------------------");
-			Console.WriteLine("     |[1]    " + this.gameFields[0] + "    |[2]    " + this.gameFields[1] + "    |[3]    " + this.gameFields[2] + "   |");
-			Console.WriteLine("     |[4]    " + this.gameFields[3] + "    |[5]    " + this.gameFields[4] + "    |[6]    " + this.gameFields[5] + "   |");
-			Console.WriteLine("     |[7]    " + this.gameFields[6] + "    |[8]    " + this.gameFields[7] + "    |[9]    " + this.gameFields[8] + "   |");
-			Console.WriteLine("     ---------------------------------------\n");
+			Console.WriteLine("     ----------------------------------------");
+			Console.WriteLine("     |[1]" + this.drawnFields[0] + "|[2]" + this.drawnFields[1] + "|[3]" + this.drawnFields[2] + "|");
+			Console.WriteLine("     |[4]" + this.drawnFields[3] + "|[5]" + this.drawnFields[4] + "|[6]" + this.drawnFields[5] + "|");
+			Console.WriteLine("     |[7]" + this.drawnFields[6] + "|[8]" + this.drawnFields[7] + "|[9]" + this.drawnFields[8] + "|");
+			Console.WriteLine("     ----------------------------------------\n");
 		}
 		
 		public void SetOpponentMode()
 		{
-			int players = int.Parse(Console.ReadLine());
+			int players = (int)validation.ValidateNumericValue(1, 2);
 			this.againstComputer = (players == 1);
 		}
 		
 		private void PlayTurnHuman(string player, char shape)
 		{
-			Console.WriteLine(player + ", escolha a posição para jogar: ");
+			Console.SetCursorPosition(0, 9);
+			Console.Write(player + ", escolha a posição para jogar: ");
 			
 			do
 			{
-				positionPlayed = int.Parse(Console.ReadLine()); // Will be changed to validation class to prevent parsing errors
+				Console.SetCursorPosition(41, 9);
+				Console.Write(" ");
+				Console.SetCursorPosition(41, 9);
+				positionPlayed = (int)validation.ValidateNumericValue(1, 9) - 1;
 			}
-			while (gameFields[positionPlayed - 1] != ' ');
+			while (gameFields[positionPlayed] != ' ');
 			
-			gameFields[positionPlayed - 1] = shape;
+			gameFields[positionPlayed] = shape;
+			drawnFields[positionPlayed] = "    " + gameFields[positionPlayed] + "    ";
 		}
 		
+		// Try to make multiple difficulties
 		private void PlayTurnComputer()
 		{
-			// To do: Computer logic
-			// Try to make multiple difficulties
+			Console.SetCursorPosition(0, 9);
+			Console.Write("Jogada do computador." + new string(' ', 30));
+			Thread.Sleep(500);
+			
+			// Easy
+			positionPlayed = -1;
+			do
+			{
+				positionPlayed++;
+			}
+			while(gameFields[positionPlayed] != ' ');
+			
+			gameFields[positionPlayed] = 'O';
+			drawnFields[positionPlayed] = "    " + gameFields[positionPlayed] + "    ";
 		}
 		
 		private void CheckForWinner()
@@ -72,19 +101,19 @@ namespace TicTacToe
 				if ((gameFields[0] == gameFields[1]) && (gameFields[1] == gameFields[2]))
 				{
 					winnerFound = true;
-					victoryCombination = 1;
+					drawnFields[0] = drawnFields[1] = drawnFields[2] = "<<<<" + gameFields[0] + ">>>>";
 				}
 				
 				if ((gameFields[0] == gameFields[3]) && (gameFields[3] == gameFields[6]))
 				{
 					winnerFound = true;
-					victoryCombination = 4;
+					drawnFields[0] = drawnFields[3] = drawnFields[6] = "<<<<" + gameFields[0] + ">>>>";
 				}
 				
 				if ((gameFields[0] == gameFields[4]) && (gameFields[4] == gameFields[8]))
 				{
 					winnerFound = true;
-					victoryCombination = 7;
+					drawnFields[0] = drawnFields[4] = drawnFields[8] = "<<<<" + gameFields[0] + ">>>>";
 				}
 			}
 			
@@ -93,19 +122,19 @@ namespace TicTacToe
 				if ((gameFields[3] == gameFields[4]) && (gameFields[4] == gameFields[5]))
 				{
 					winnerFound = true;
-					victoryCombination = 2;
+					drawnFields[3] = drawnFields[4] = drawnFields[5] = "<<<<" + gameFields[4] + ">>>>";
 				}
 				
 				if ((gameFields[1] == gameFields[4]) && (gameFields[4] == gameFields[7]))
 				{
 					winnerFound = true;
-					victoryCombination = 5;
+					drawnFields[1] = drawnFields[4] = drawnFields[7] = "<<<<" + gameFields[4] + ">>>>";
 				}
 				
 				if ((gameFields[2] == gameFields[4]) && (gameFields[4] == gameFields[6]))
 				{
 					winnerFound = true;
-					victoryCombination = 8;
+					drawnFields[2] = drawnFields[4] = drawnFields[6] = "<<<<" + gameFields[4] + ">>>>";
 				}
 			}
 			
@@ -114,18 +143,16 @@ namespace TicTacToe
 				if ((gameFields[6] == gameFields[7]) && (gameFields[7] == gameFields[8]))
 				{
 					winnerFound = true;
-					victoryCombination = 3;
+					drawnFields[6] = drawnFields[7] = drawnFields[8] = "<<<<" + gameFields[8] + ">>>>";
 				}
 				
 				if ((gameFields[2] == gameFields[5]) && (gameFields[5] == gameFields[8]))
 				{
 					winnerFound = true;
-					victoryCombination = 6;
+					drawnFields[2] = drawnFields[5] = drawnFields[8] = "<<<<" + gameFields[8] + ">>>>";
 				}
 			}
 		}
-		
-		// To do: Method that shows the winning line instead of explicitly saying it in the program
 		
 		public void PlayGame()
 		{
@@ -138,7 +165,8 @@ namespace TicTacToe
 				
 				if (currentTurn > 9)
 				{
-					Console.WriteLine("Empate");
+					Console.SetCursorPosition(21, 14);
+					Console.Write("Empate!");
 					break;
 				}
 				
@@ -152,7 +180,7 @@ namespace TicTacToe
 				{
 					if (againstComputer)
 					{
-						
+						PlayTurnComputer();
 					}
 					else
 					{
@@ -165,21 +193,27 @@ namespace TicTacToe
 					CheckForWinner();
 				}
 				
-				DrawPlayField();
+				RedrawPlayField();
 			}
 			
 			if (winnerFound)
 			{
+				Console.SetCursorPosition(16, 14);
 				if (isPlayerOneTurn)
 				{
-					Console.WriteLine("Jogador 1 venceu!");
+					Console.Write("Jogador 1 venceu!");
 				}
 				else
 				{
-					Console.WriteLine("Jogador 2 venceu!");
+					if (againstComputer)
+					{
+						Console.Write("Computador venceu!");
+					}
+					else
+					{
+						Console.Write("Jogador 2 venceu!");
+					}
 				}
-				
-				Console.WriteLine("Linha da vitória: " + victoryCombination);
 			}
 		}
 	}

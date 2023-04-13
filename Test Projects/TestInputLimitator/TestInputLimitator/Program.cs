ac26fc684d4ca0a3ce4eafb82f85607db6075548
Edinaldo C da Silva Jr <edinaldo.csilvajr@outlook.com>
2023-04-13 20:07:39 -0300

    Started a new class library: InputLimitator. It works similarly to InputValidation, but putting a hard limit on how many characters can be entered (and it can also surpass the 254 character limit of a Console.Readline()). Also created a test project to test it.

---
+++ Test Projects/TestInputLimitator/TestInputLimitator/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Date: 13/04/2023
+ * Time: 19:31
+*/
+using System;
+using InputLimitator;
+
+namespace TestInputLimitator
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			double number;
+			
+			LimitInput limitator = new LimitInput();
+			
+			Console.WriteLine("Digite number número: ");
+			number = limitator.LimitInputNumberOnly(10, 30);
+			Console.WriteLine("\n" + number);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ _Class Libraries_/InputLimitator/InputLimitator/LimitInput.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Date: 12/04/2023
+ * Time: 18:11
+ */
+using System;
+using System.Threading;
+using System.Media;
+using System.Collections.Generic;
+
+namespace InputLimitator
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class LimitInput
+	{
+		private string fullInput = "";
+		private ConsoleKeyInfo currentCharInput;
+		private int inputAscii, cursorX, cursorY;
+		
+		public double LimitInputNumberOnly (int maxInputSize, int minInputSize)
+		{	
+			cursorX = Console.CursorLeft;
+			cursorY = Console.CursorTop;
+			
+			if (maxInputSize < minInputSize)
+			{
+				int auxInput = minInputSize;
+				minInputSize = maxInputSize;
+				maxInputSize = auxInput;
+			}
+			
+			do
+			{
+				currentCharInput = Console.ReadKey(true);
+				inputAscii = (int)currentCharInput.KeyChar;
+				
+				if (inputAscii == 13)
+				{
+					if (fullInput.Length < minInputSize)
+					{
+						SystemSounds.Beep.Play();
+					}
+				}
+				else
+				{
+					if (inputAscii == 8)
+					{
+						if (fullInput.Length > 0)
+						{
+							fullInput = fullInput.Substring(0, fullInput.Length - 1);
+							Console.SetCursorPosition(cursorX, cursorY);
+							Console.Write(new String(' ', fullInput.Length + 1));
+						}
+						else
+						{
+							SystemSounds.Beep.Play();
+						}
+					}
+					else
+					{
+						if ((inputAscii > 47 && inputAscii < 58) && (fullInput.Length < maxInputSize))
+						{
+							fullInput += currentCharInput.KeyChar;
+						}
+						else
+						{
+							SystemSounds.Beep.Play();
+						}
+					}
+				}
+				
+				Console.SetCursorPosition(cursorX, cursorY);
+				Console.Write(fullInput);
+			}
+			while ((currentCharInput.Key != ConsoleKey.Enter) || (fullInput.Length < minInputSize));
+			
+			return double.Parse(fullInput);
+		}
+		
+	}
+}

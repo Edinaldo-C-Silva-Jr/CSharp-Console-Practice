ae9c2ceb09d6ad7096b35993dd084848c82075ce
Edinaldo C da Silva Jr <edinaldo.csilvajr@outlook.com>
2023-04-15 16:01:09 -0300

    Made a quick test project to help identifying keys and their values.

---
+++ Test Projects/TestKeyInfo/TestKeyInfo/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Date: 15/04/2023
+ * Time: 15:13
+*/
+using System;
+
+namespace TestKeyInfo
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			ConsoleKeyInfo keyInfo;
+			int asciiValue;
+			
+			Console.WriteLine("Press any key to see its information: ");
+			
+			while (true) {
+				keyInfo = Console.ReadKey(true);
+				asciiValue = (int)keyInfo.KeyChar;
+				
+				Console.SetCursorPosition(0, 2);
+				Console.Write(new String(' ', 320));
+				Console.SetCursorPosition(0, 2);
+				Console.Write("Key: " + keyInfo.Key + "\nCharacter: " + keyInfo.KeyChar + "\nModifier: " +  keyInfo.Modifiers + "\nValue: " + asciiValue);
+			}
+		}
+	}
+}

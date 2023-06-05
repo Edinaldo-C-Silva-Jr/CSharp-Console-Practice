15e98f7cd71ab776059160ee4e35cc115e47a4ea
Edinaldo C da Silva Jr <edinaldo.csilvajr@outlook.com>
2023-06-05 01:51:11 -0300

    Started a Console MineSweeper project. Implemented a MineFieldCell class, to represent each cell of the grid, and implemented the logic for each cell to find out the amount of mines around it.

---
+++ MineSweeper/MineSweeper/MineFieldCell.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Date: 04/06/2023
+ * Time: 23:34
+*/
+using System;
+
+namespace MineSweeper
+{
+	/// <summary>
+	/// Description of MineFieldCell.
+	/// </summary>
+	public class MineFieldCell
+	{
+		private int xPosition, yPosition, neighborMinesAmount;
+		private bool cellIsMine;
+		
+		public MineFieldCell(int x, int y, bool mine)
+		{
+			this.xPosition = x;
+			this.yPosition = y;
+			this.cellIsMine = mine;
+		}
+		
+		public int GetX()
+		{
+			return this.xPosition;
+		}
+		
+		public int GetY()
+		{
+			return this.yPosition;
+		}
+		
+		public char GetNumber()
+		{
+			if (cellIsMine)
+			{
+				return 'M';
+			}
+			else
+			{
+				return neighborMinesAmount.ToString()[0];
+			}
+		}
+		
+		public bool IsMine()
+		{
+			return this.cellIsMine;
+		}
+		
+		public void CheckNeighboringCell(MineFieldCell neighbor)
+		{
+			if (neighbor.IsMine())
+			{
+				neighborMinesAmount++;
+			}
+		}
+	}
+}
---
+++ MineSweeper/MineSweeper/Program.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * Date: 04/06/2023
+ * Time: 23:32
+ */
+using System;
+
+namespace MineSweeper
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Random minePicker = new Random();
+			MineFieldCell[,] mineField = new MineFieldCell[10,10];
+			
+			for (int x = 0; x < 10; x++)
+			{
+				for (int y = 0; y < 10; y++)
+				{
+					mineField[x,y] = new MineFieldCell(x, y, Convert.ToBoolean(minePicker.Next(2)));
+				}
+			}
+			
+			for (int x = 0; x < 10; x++)
+			{
+				for (int y = 0; y < 10; y++)
+				{
+					if (x != 0)
+					{
+						mineField[x,y].CheckNeighboringCell(mineField[x - 1, y]);
+					}
+					
+					if (x != 9)
+					{
+						mineField[x,y].CheckNeighboringCell(mineField[x + 1, y]);
+					}
+					
+					if (y != 0)
+					{
+						mineField[x,y].CheckNeighboringCell(mineField[x, y - 1]);
+					}
+					
+					if (y != 9)
+					{
+						mineField[x,y].CheckNeighboringCell(mineField[x, y + 1]);
+					}
+					
+					if (x != 0 && y != 0)
+					{
+						mineField[x,y].CheckNeighboringCell(mineField[x - 1, y - 1]);
+					}
+					
+					if (x != 0 && y != 9)
+					{
+						mineField[x,y].CheckNeighboringCell(mineField[x - 1, y + 1]);
+					}
+					
+					if (x != 9 && y != 0)
+					{
+						mineField[x,y].CheckNeighboringCell(mineField[x + 1, y - 1]);
+					}
+					
+					if (x != 9 && y != 9)
+					{
+						mineField[x,y].CheckNeighboringCell(mineField[x + 1, y + 1]);
+					}
+				}
+			}
+			
+			for (int x = 0; x < 10; x++)
+			{
+				Console.WriteLine(" ");
+				
+				for (int y = 0; y < 10; y++)
+				{
+					Console.Write(" " + mineField[x,y].GetNumber());
+				}
+			}
+			
+			Console.ReadKey();
+		}
+	}
+}

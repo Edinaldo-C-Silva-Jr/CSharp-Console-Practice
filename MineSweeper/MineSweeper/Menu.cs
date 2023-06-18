bfea6d127fb807ba7452a8ca04940a022b901e4e
Edinaldo C da Silva Jr <edinaldo.csilvajr@outlook.com>
2023-06-17 22:08:14 -0300

    Improved the menu considerably! It now opens a secondary 'menu' to pick from the different available options for the size of the mine field and the amount of mines.

--- MineSweeper/MineSweeper/Menu.cs
+++ MineSweeper/MineSweeper/Menu.cs
@@ -63,6 +63,144 @@ namespace MineSweeper
 		}
 		#endregion
 		
+		private void DrawOptionChoiceWindow(int amountOfOptions)
+		{
+			Console.SetCursorPosition(0, 9);
+			Console.Write("   Choose the desired amount from the available options:\n     ");
+			
+			for (int i = 1; i < amountOfOptions + 1; i++)
+			{
+				if (i < 2)
+				{
+					Console.Write("00" + 5*i + "  ");
+				}
+				else
+				{
+					if (i < 20)
+					{
+						Console.Write("0" + 5*i + "  ");
+					}
+					else
+					{
+						Console.Write(5*i + "  ");
+					}
+				}
+				
+				if (i % 10 == 0)
+				{
+					Console.Write("\n     ");
+				}
+			}
+		}
+		
+		private void DrawOptionChoiceSelectionCursor(int currentlySelectedOption)
+		{
+			Console.SetCursorPosition((currentlySelectedOption % 10 + 1) * 5 - 1, (currentlySelectedOption / 10 + 10));
+			Console.Write(">");
+			Console.SetCursorPosition((currentlySelectedOption % 10 + 1) * 5 + 3, (currentlySelectedOption / 10 + 10));
+			Console.Write("<");
+		}
+		
+		private void EraseOptionChoiceSelectionCursor(int currentlySelectedOption)
+		{
+			Console.SetCursorPosition((currentlySelectedOption % 10 + 1) * 5 - 1, (currentlySelectedOption / 10 + 10));
+			Console.Write(" ");
+			Console.SetCursorPosition((currentlySelectedOption % 10 + 1) * 5 + 3, (currentlySelectedOption / 10 + 10));
+			Console.Write(" ");
+		}
+		
+		private int ControlOptionChoiceWindow(int maxAmountOfOptions)
+		{
+			ConsoleKey optionInput;
+			int currentOptionChoice = 0;
+			DrawOptionChoiceSelectionCursor(0);
+			
+			do
+			{
+				optionInput = Console.ReadKey(true).Key;
+				EraseOptionChoiceSelectionCursor(currentOptionChoice);
+				
+				switch(optionInput)
+				{
+					case ConsoleKey.UpArrow: // Up arrow pressed
+						{
+							if (currentOptionChoice > 9) // If not at the top, then move one option up
+							{
+								currentOptionChoice -= 10;
+							}
+							break;
+						}
+					case ConsoleKey.DownArrow: // Down arrow pressed
+						{
+							if (currentOptionChoice < maxAmountOfOptions - 10) // If not at the bottom, then move one option down
+							{
+								currentOptionChoice += 10;
+							}
+							break;
+						}
+					case ConsoleKey.LeftArrow: // Left arrow pressed
+						{
+							if (currentOptionChoice % 10 != 0) // If not at the left, then move one option left
+							{
+								currentOptionChoice--;
+							}
+							break;
+						}
+					case ConsoleKey.RightArrow: // Right arrow pressed
+						{
+							if (currentOptionChoice % 10 != 9 && currentOptionChoice != maxAmountOfOptions - 1) // If not at the right edge or at maximum option available, then move one option right
+							{
+								currentOptionChoice++;
+							}
+							break;
+						}
+					default: // Any other key is pressed
+						{
+							SystemSounds.Beep.Play();
+							break;
+						}
+				}
+				
+				DrawOptionChoiceSelectionCursor(currentOptionChoice);
+			}
+			while (optionInput != ConsoleKey.Enter); // Keep going until Esc is pressed
+			
+			Console.SetCursorPosition(0, 9);
+			Console.Write(new String(' ', 660));
+			
+			return (currentOptionChoice + 1) * 5;
+		}
+		
+		private void ChangeHorizontalSize()
+		{
+			DrawOptionChoiceWindow(8);
+			xSize = ControlOptionChoiceWindow(8);
+			
+			Console.SetCursorPosition(29, 3);
+			Console.Write(xSize + " ");
+			CheckMineCount();
+		}
+		
+		private void ChangeVerticalSize()
+		{
+			DrawOptionChoiceWindow(5);
+			ySize = ControlOptionChoiceWindow(5);
+			
+			Console.SetCursorPosition(27, 4);
+			Console.Write(ySize + " ");
+			CheckMineCount();
+		}
+		
+		private void ChangeMineAmount()
+		{
+			DrawOptionChoiceWindow(ySize * xSize / 10);
+			mineCount = ControlOptionChoiceWindow(ySize * xSize / 10);
+			
+			Console.SetCursorPosition(29, 5);
+			Console.Write(mineCount + " ");
+			CheckMineCount();
+		}
+		
 		private void CheckMineCount()
 		{
 			if (mineCount > xSize * ySize / 2)
@@ -127,46 +265,17 @@ namespace MineSweeper
 									}
 								case 1: // Second option: Set horizontal size of minefield
 									{
-										if (xSize < 40) // Size can't be bigger than 40
-										{
-											xSize += 5;
-										}
-										else
-										{
-											xSize = 5;
-										}
-										Console.SetCursorPosition(29, 3);
-										Console.Write(xSize + " ");
-										CheckMineCount();
+										ChangeHorizontalSize();
 										break;
 									}
 								case 2: // Third option: Set vertical size of minefield
 									{
-										if (ySize < 25) // Size can't be bigger than 25
-										{
-											ySize += 5;
-										}
-										else
-										{
-											ySize = 5;
-										}
-										Console.SetCursorPosition(27, 4);
-										Console.Write(ySize + " ");
-										CheckMineCount();
+										ChangeVerticalSize();
 										break;
 									}
 								case 3: // Fourth option: Set mine amount
 									{
-										if (mineCount + 5 <= (xSize * ySize) / 2)
-										{
-											mineCount += 5;
-										}
-										else
-										{
-											mineCount = 5;
-										}
-										Console.SetCursorPosition(29, 5);
-										Console.Write(mineCount + "   ");
+										ChangeMineAmount();
 										break;
 									}
 							}

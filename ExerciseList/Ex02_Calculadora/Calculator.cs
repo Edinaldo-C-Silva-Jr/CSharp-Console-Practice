4cabf1e2ae86293fdfbcda7bea8f29123f1aef56
Edinaldo C da Silva Jr <edinaldo.csilvajr@outlook.com>
2023-08-26 15:26:01 -0300

    Finished the first exercise: Calculator.

---
+++ ExerciseList/Ex02_Calculadora/Calculator.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Date: 26/08/2023
+ * Time: 14:41
+ */
+using System;
+
+namespace Ex02_Calculadora
+{
+	/// <summary>
+	/// Description of Calculator.
+	/// </summary>
+	public class Calculator
+	{
+		double n1, n2;
+		
+		public Calculator()
+		{
+			
+		}
+		
+		public void ReceiveInput()
+		{
+			Console.Clear();
+			Console.Write("Digite o primeiro número: ");
+			n1 = double.Parse(Console.ReadLine());
+			Console.Write("Digite o segundo número: ");
+			n2 = double.Parse(Console.ReadLine());
+		}
+		
+		public void ReceiveInputDivision()
+		{
+			Console.Clear();
+			Console.Write("Digite o primeiro número: ");
+			n1 = double.Parse(Console.ReadLine());
+			
+			do
+			{
+				Console.Write("Digite o segundo número: ");
+				n2 = double.Parse(Console.ReadLine());
+				if (n2 == 0)
+				{
+					Console.SetCursorPosition(0, 6);
+					Console.WriteLine("Não é possível dividir por 0.");
+					Console.SetCursorPosition(0, 1);
+					Console.Write(new String(' ', 50));
+					Console.SetCursorPosition(0, 1);
+				}
+			}
+			while(n2 == 0);
+		}
+		
+		
+		public double Addition ()
+		{
+			ReceiveInput();
+			return n1 + n2;
+		}
+		
+		public double Subtraction ()
+		{
+			ReceiveInput();
+			return n1 - n2;
+		}
+		
+		public double Multiplication()
+		{
+			ReceiveInput();
+			return n1 * n2;
+		}
+		
+		public double Division()
+		{
+			ReceiveInputDivision();
+			return n1 / n2;
+		}
+	}
+}
--- ExerciseList/Ex02_Calculadora/Program.cs
+++ ExerciseList/Ex02_Calculadora/Program.cs
@@ -1,21 +1,85 @@
 ﻿/*
  * Date: 26/08/2023
  * Time: 14:36
-*/
+ */
 using System;
+using System.Diagnostics;
 
 namespace Ex02_Calculadora
 {
 	class Program
 	{
+		public static void DrawMenu()
+		{
+			Console.Clear();
+			Console.SetCursorPosition(23, 0);
+			Console.Write("Menu");
+			Console.SetCursorPosition(10, 2);
+			Console.Write("1 - Adição");
+			Console.SetCursorPosition(10, 3);
+			Console.Write("2 - Subtração");
+			Console.SetCursorPosition(10, 4);
+			Console.Write("3 - Multiplicação");
+			Console.SetCursorPosition(10, 5);
+			Console.Write("4 - Divisão");
+			Console.SetCursorPosition(10, 9);
+			Console.Write("0 - Sair");
+			
+			Console.SetCursorPosition(5, 12);
+			Console.Write("Escolha uma opção: ");
+		}
+		
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.SetWindowSize(50, 15);
+			Console.SetBufferSize(50, 15);
 			
-			// TODO: Implement Functionality Here
+			Calculator calculate = new Calculator();
+			char option;
 			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
+			do
+			{
+				DrawMenu();
+				option = Console.ReadKey(false).KeyChar;
+				
+				switch(option)
+				{
+					case '0':
+						{
+							Console.Clear();
+							Console.Write("Saindo do aplicativo...");
+							break;
+						}
+					case '1':
+						{
+							Console.Write("Resultado: " + calculate.Addition());
+							break;
+						}
+					case '2':
+						{
+							Console.Write("Resultado: " + calculate.Subtraction());
+							break;
+						}
+					case '3':
+						{
+							Console.Write("Resultado: " + calculate.Multiplication());
+							break;
+						}
+					case '4':
+						{
+							Console.Write("Resultado: " + calculate.Division());
+							break;
+						}
+					default:
+						{
+							Console.Clear();
+							Console.Write("Opção Inválida.");
+							break;
+						}
+				};
+				Console.ReadLine();
+			}
+			while(option != '0');
 		}
 	}
 }

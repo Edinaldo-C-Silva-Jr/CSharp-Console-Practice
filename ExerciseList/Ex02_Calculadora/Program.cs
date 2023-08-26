14320bf4b3b0db3e18f1c4bad027cd939d29a781
Edinaldo C da Silva Jr <edinaldo.csilvajr@outlook.com>
2023-08-26 14:40:35 -0300

    Started resolving an exercise list. Created the solution and the project for the first exercise.

---
+++ ExerciseList/Ex02_Calculadora/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Date: 26/08/2023
+ * Time: 14:36
+*/
+using System;
+
+namespace Ex02_Calculadora
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}

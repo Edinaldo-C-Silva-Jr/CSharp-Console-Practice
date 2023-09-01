ce9c0880780fac9eeb533e352dc86e50eb9593da
Edinaldo C da Silva Jr <edinaldo.csilvajr@outlook.com>
2023-08-31 21:46:14 -0300

    Fixed an oversight on the folder structure. Got started on the fifth exercise: ConversaoMoeda. Made a barebones, yet functional version of it.

---
+++ Lista de Exercicios - Wurthman/ExerciseList/Ex06_ConversaoMoedas/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Date: 31/08/2023
+ * Time: 21:04
+*/
+using System;
+
+namespace Ex06_ConversaoMoedas
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			decimal valorReal = 0, valorConvertido = 0;
+			byte moeda;
+			
+			Console.WriteLine("Digite o valor que deseja converter, em reais (R$): ");
+			decimal.TryParse(Console.ReadLine(), out valorReal);
+			
+			Console.WriteLine("Para qual moeda você deseja converter o valor? ");
+			Console.WriteLine("1 - Dólar($)");
+			Console.WriteLine("2 - Euro (€)");
+			Console.WriteLine("3 - Iene (¥)");
+			Console.WriteLine("4 - Libra esterlina (£)");
+			byte.TryParse(Console.ReadLine(), out moeda);
+			
+			switch(moeda)
+			{
+				case 1:
+					{
+						valorConvertido = valorReal / 4.5M;
+						break;
+					}
+				case 2:
+					{
+						valorConvertido = valorReal / 6.2M;
+						break;
+					}
+				case 3:
+					{
+						valorConvertido = valorReal / 0.052M;
+						break;
+					}
+				case 4:
+					{
+						valorConvertido = valorReal / 6.95M;
+						break;
+					}
+				default:
+					{
+						break;
+					}
+			}
+			
+			Console.WriteLine(valorConvertido.ToString());
+			Console.ReadLine();
+		}
+	}
+}

9b4a7ca235f93991009a1b125837d67c2eb03c4e
Edinaldo C da Silva Jr <edinaldo.csilvajr@outlook.com>
2023-09-16 20:33:24 -0300

    Made a test run of how to read files in C#

--- Lista de Exercicios - Wurthman/ExerciseList/Ex07_JogoForca/Program.cs
+++ Lista de Exercicios - Wurthman/ExerciseList/Ex07_JogoForca/Program.cs
@@ -3,6 +3,9 @@
  * Time: 20:57
 */
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 
 namespace Ex07_JogoForca
 {
@@ -10,7 +13,31 @@ namespace Ex07_JogoForca
 	{
 		public static void Main(string[] args)
 		{
+			List<string> word = new List<string>(), type = new List<string>();
+			string[] lineSplit;
 			
+			string line;
+			string path = "C:\\Users\\Windows\\Documents\\SharpDevelop Projects\\C# Console Practice\\Lista de Exercicios - Wurthman\\ExerciseList\\Ex07_JogoForca\\Resources\\WordList.csv";
+			StreamReader wordRead = new StreamReader(path);
+			
+			while((line = wordRead.ReadLine()) != null)
+			{
+				lineSplit = line.Split(',');
+				word.Add(lineSplit[0]);
+				type.Add(lineSplit[1]);
+			}
+			
+			foreach(string s in word)
+			{
+				Console.WriteLine(s);
+			}
+			
+			foreach(string s in type)
+			{
+				Console.WriteLine(s);
+			}
+			
+			Console.ReadKey();
 		}
 	}
 }

0b1e65947d47ded0706713d6867c06f4c5910d70
Edinaldo C da Silva Jr <edinaldo.csilvajr@outlook.com>
2023-11-19 16:23:30 -0300

    Added a new class GameSetup to better organize the setup code. Changed the word list to english.

---
+++ Lista de Exercicios - Wurthman/ExerciseList/Ex07_JogoForca/GameSetup.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * Date: 19/11/2023
+ * Time: 15:59
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Ex07_JogoForca.Resources
+{
+	/// <summary>
+	/// Description of GameSetup.
+	/// </summary>
+	public class GameSetup
+	{
+		private Random randomNumber;
+		private List<string> listOfWords;
+		private List<Category> listOfCategories;
+		
+		public GameSetup()
+		{
+			randomNumber = new Random();
+		}
+		
+		public void ReadWordListFile()
+		{
+			listOfWords = new List<string>();
+			listOfCategories = new List<Category>();
+			
+			string line;
+			string[] lineSplit;
+			
+			string path = AppDomain.CurrentDomain.BaseDirectory;
+			path = path.TrimEnd(new char[]{'\\', 'b', 'i', 'n', 'D', 'e', 'u', 'g'});
+			path += "\\Resources\\WordList.csv";
+			
+			using (StreamReader wordFileReader = new StreamReader(path))
+			{
+				while((line = wordFileReader.ReadLine()) != null)
+				{
+					lineSplit = line.Split(',');
+					listOfWords.Add(lineSplit[0]);
+					
+					Category currentWordCategory;
+					Enum.TryParse(lineSplit[1], out currentWordCategory);
+					listOfCategories.Add(currentWordCategory);
+				}
+			}
+			
+			
+		}
+		
+		public Category PickCategory()
+		{
+			Category chosenCategory = (Category)randomNumber.Next(0, 2);
+			return chosenCategory;
+		}
+		
+		public string PickWord(Category chosenCategory)
+		{
+			List<int> wordsInChosenCategory = new List<int>();
+			
+			for (int i = 0; i < listOfCategories.Count; i++)
+			{
+				if (listOfCategories[i] == chosenCategory)
+				{
+					wordsInChosenCategory.Add(i);
+				}
+			}
+			
+			int randomWord = wordsInChosenCategory[randomNumber.Next(0, wordsInChosenCategory.Count)];
+			return listOfWords[randomWord];
+		}
+	}
+}
--- Lista de Exercicios - Wurthman/ExerciseList/Ex07_JogoForca/Program.cs
+++ Lista de Exercicios - Wurthman/ExerciseList/Ex07_JogoForca/Program.cs
@@ -3,8 +3,7 @@
  * Time: 20:57
  */
 using System;
-using System.Collections.Generic;
-using System.IO;
+using Ex07_JogoForca.Resources;
 
 namespace Ex07_JogoForca
 {
@@ -12,48 +11,14 @@ namespace Ex07_JogoForca
 	{
 		public static void Main(string[] args)
 		{
-			Random wordPicker = new Random();
-			List<string> words = new List<string>();
-			List<Category> types = new List<Category>();
-			string[] lineSplit;
+			GameSetup setupGame = new GameSetup();
 			
-			// Read the file
 			
-			string line;
-			string path = AppDomain.CurrentDomain.BaseDirectory;
-			path = path.TrimEnd(new char[]{'\\', 'b', 'i', 'n', 'D', 'e', 'u', 'g'});
-			path += "\\Resources\\WordList.csv";
-			StreamReader wordRead = new StreamReader(path);
+			setupGame.ReadWordListFile();
+			Category wordCategory = setupGame.PickCategory();
+			string chosenWord = setupGame.PickWord(wordCategory);
 			
-			while((line = wordRead.ReadLine()) != null)
-			{
-				lineSplit = line.Split(',');
-				words.Add(lineSplit[0]);
-				
-				Category type;
-				Enum.TryParse(lineSplit[1], out type);
-				types.Add(type);
-			}
-			
-			// Pick category
-			
-			Category category = (Category)wordPicker.Next(0, 2);
-			List<int> wordsInCategory = new List<int>();
-			
-			for (int i = 0; i < types.Count; i++)
-			{
-				if (types[i] == category)
-				{
-					wordsInCategory.Add(i);
-				}
-			}
-			
-			int randomWord = wordsInCategory[wordPicker.Next(0, wordsInCategory.Count)];
-			string pickedWord = words[randomWord];
-			
-			// Start Game
-			
-			HangmanGame game = new HangmanGame(category, pickedWord);
+			HangmanGame game = new HangmanGame(wordCategory, chosenWord);
 		}
 	}
 }
